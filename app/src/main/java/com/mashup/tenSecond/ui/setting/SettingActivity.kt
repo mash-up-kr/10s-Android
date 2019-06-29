a4ba947aefff676088cab101834cf4695d895b7e
namget <yeboykr@naver.com>
2019-06-29 20:54:56 +0900

    1. 바텀네비게이션 수정 2. 베이스액티비티, 베이스프래그먼트 수정 3. 채팅리스트 어뎁터 추가 4. setting 액티비티로 변경

--- app/src/main/java/com/mashup/tenSecond/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/mashup/tenSecond/ui/setting/SettingActivity.kt
@@ -1,5 +1,6 @@
 package com.mashup.tenSecond.ui.setting
 
+import android.os.Bundle
 import com.mashup.tenSecond.R
 import com.mashup.tenSecond.databinding.ActivitySettingBinding
 import com.namget.diaryLee.ui.base.BaseActivity
@@ -8,17 +9,8 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>() {
 
     override fun onLayoutId(): Int = R.layout.activity_setting
 
-    companion object {
-        var settingActivity: SettingActivity? = null
 
-        fun getInstance(): SettingActivity {
-            if (settingActivity == null) {
-                synchronized(SettingActivity::class) {
-                    settingActivity = SettingActivity()
-                }
-            }
-            return settingActivity as SettingActivity
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
-
 }
